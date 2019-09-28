d04c31777f0190174fdc1d3dbdf83d35835130ca
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-09-28 11:50:37 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,55 +1,78 @@
-const header = new Headers({
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': '*'
-})
+function saveCity(cityname){
+  localStorage.setItem('city', city.value)
+  console.log(city.value)
+  hideForm();
+};
 
+function changeCity(){
+  localStorage.removeItem('city')
+  document.querySelectorAll('[class=nocity]').forEach(element=>
+  element.style.display= "inline");
+  document.querySelectorAll('[id=yescity]').forEach(element=>
+  element.style.display= "none");
+};
 
-const vote_url = new URL('https://sf-pyw.mosyag.in/sse/vote/')
-const results = new URL('https://sf-pyw.mosyag.in/sse/vote/stats')
+function hideForm(){
+  document.querySelectorAll('[class=nocity]').forEach(element=>
+  element.style.display= "none");
+  document.querySelectorAll('[id=yescity]').forEach(element=>
+  element.style.display= "inline");
+  var element = document.getElementById("yescity");
+  element.innerHTML = 'Ваш город — ' + localStorage.getItem('city');
+};
 
+function init(){
+  console.log("page loaded")
+  if (localStorage.getItem('city') == null) {
+    console.log("no city stored");
+  } else{
+    hideForm();
+  }
+};
 
-const RES = new EventSource(results, header)
-RES.onmessage = message => {
-  //console.log(message.data);
-  var votes = JSON.parse(message.data);
-  total_votes = votes["cats"] + votes["dogs"] + votes["parrots"];
+function checkboxToArray(){
+    var arr= new Array();
+    document.querySelectorAll('[class=checkbox]').forEach(element=>{
+        var x = document.getElementById(element.id).checked;
+        if (x == true) {
+          arr.push(element.id);
+        };
+      });
+    return arr;
+};
 
-  //считаем котов
-  votes_cats = (votes["cats"] / total_votes) * 100;
-  votes_cats = votes_cats.toFixed(2);
-  const progress_cats = document.querySelector('.progress-bar-success')
-  progress_cats.style.cssText = `width: ${votes_cats}%;`
-  progress_cats.textContent = `${votes_cats}% (${votes["cats"]})`
+function storeCookie(){
+  var arr = checkboxToArray()
+  var json_str = JSON.stringify(arr);
+  createCookie('checkboxcookie', json_str, 3600);
 
-  //считаем собак
-  votes_dogs = (votes["dogs"] / total_votes) * 100;
-  votes_dogs = votes_dogs.toFixed(2);
-  const progress_dogs = document.querySelector('.progress-bar-info')
-  progress_dogs.style.cssText = `width: ${votes_dogs}%;`
-  progress_dogs.textContent = `${votes_dogs}% (${votes["dogs"]})`
-
-  //считаем попугаев
-  votes_parrots = (votes["parrots"] / total_votes) * 100;
-  votes_parrots = votes_parrots.toFixed(2);
-  const progress_parrots = document.querySelector('.progress-bar-warning')
-  progress_parrots.style.cssText = `width: ${votes_parrots}%;`
-  progress_parrots.textContent = `${votes_parrots}% (${votes["parrots"]})`
 }
 
-function postResult(vote){
-  //генерируем пустой ПОСТ запрос
-  var xhr = new XMLHttpRequest();
-  open_url = vote_url + vote;
-  console.log(open_url);
-  xhr.open("POST", open_url, true);
-  xhr.send('');
-  //делаем кнопки не активными
-  document.querySelectorAll('[id=btn]').forEach(element=>
-  element.disabled = true);
-  //показываем бары голосования
-  document.querySelectorAll('[class=progress]').forEach(element=>
-  element.style.visibility= "visible");
+function createCookie(cookiename, cookievalue, hours) {
+    document.cookie = "user=Max; max-age=3500";
+    document.cookie = "\"" + encodeURIComponent(cookiename)+ "=" + encodeURIComponent(cookievalue)+"; max-age=0";
+    var date = new Date();
+    date.setTime(date.getTime() + Number(hours) * 3600 * 1000);
+    var cookie = encodeURIComponent(cookiename) + "=" + encodeURIComponent(cookievalue) + "; expires = " + date.toGMTString();
+    //var cookie = "\"" + encodeURIComponent(cookiename) + "=" + encodeURIComponent(cookievalue) + "; expires = " + date.toGMTString() + "; path=/\"";
+    console.log(cookie)
+    document.cookie = cookie;
+
+    console.log(document.cookie.split(';'));
+
+
+}
 
 
 
-};
+function checkCookie() {
+  var user = getCookie("checkboxcookie");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
